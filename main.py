cdf91f6f42af173a6e2673d0f45cc23233121ee4
Bhavleen Kaur <107846946+Bhavleenk@users.noreply.github.com>
2023-03-17 05:40:41 +0000

    Initial

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ computer_wins=0
 while True:
   user_input=input("Type Rock/Paper/Scissors or Q to quit: ").lower()
   if user_input == "q":
-    quit()
+    break
+    #quit()
 
   if user_input not in ["rock","paper","scissors"]:
+    continue
+
+  random_number = random.randint(0,2)
+  
+
+  print("Goodbye!")
+  
     
   
