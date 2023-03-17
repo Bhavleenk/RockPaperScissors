44561b3fae999ce4ab762243b62ca6eb76a0556e
Bhavleen Kaur <107846946+Bhavleenk@users.noreply.github.com>
2023-03-17 05:45:47 +0000

    Initial

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 user_wins=0
 computer_wins=0
+options = ["rock","paper","scissors"]
 
 while True:
   user_input=input("Type Rock/Paper/Scissors or Q to quit: ").lower()
@@ -9,11 +10,16 @@ while True:
     break
     #quit()
 
-  if user_input not in ["rock","paper","scissors"]:
+  if user_input not in options:
     continue
 
   random_number = random.randint(0,2)
-  
+  #r:0, p:1, s:2
+  computer_pick = optins[random_number]
+  print("Computer picked", computer_pick, + ".")
+
+  if user_input == 'rock' and computer_pick == ' scissor':
+    print("You won!")
 
   print("Goodbye!")
   
