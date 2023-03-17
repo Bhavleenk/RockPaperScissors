763d643eb639de98a3e46b92fa8a283d7465736a
Bhavleen Kaur <107846946+Bhavleenk@users.noreply.github.com>
2023-03-17 05:38:48 +0000

    Initial

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ user_wins=0
 computer_wins=0
 
 while True:
-  
+  user_input=input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+  if user_input == "q":
+    quit()
+
+  if user_input not in ["rock","paper","scissors"]:
+    
   
